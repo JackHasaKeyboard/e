1b508781559b12da1a30e265416171e013916e29
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-10-26 20:11:42 -0600

    Almost there

---
+++ 41.hs
@@ -0,0 +1,22 @@
+prime :: Int -> Bool
+prime n = null [
+	x |
+	x <- [2..n - 1],
+	n `mod` x == 0
+	]
+
+primes = [
+	x |
+	x <- [1..],
+	prime x
+	]
+
+sumPrimes = [
+	sum $ take x primes |
+	x <- primes,
+	prime $ sum $ take x primes,
+	(sum $ take x primes) < 1000
+	]
+
+main = do
+	print sumPrimes
