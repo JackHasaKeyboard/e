b4f6b801dbc701918c1788df6cc72d13a6c45b8f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-11-10 02:55:06 -0700

    20% of the way there

---
+++ 87.hs
@@ -0,0 +1,7 @@
+prime :: Int -> Bool
+prime n = null [x | x <- [1..(n `div` 2) - 1], n `mod` x == 0]
+
+pow :: Int -> Int -> Bool
+pow n exp = null [x | x <- [1..n], x * exp == n]
+
+main = print $ pow 9 2
