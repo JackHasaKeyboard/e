514ca881bb929c9675e1c1c5e3edf19da89a66b1
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-12-10 22:37:03 -0700

    10% of the way there

---
+++ 90.hs
@@ -0,0 +1,32 @@
+import Data.List
+
+cube0 = [
+	0,
+	5,
+	6,
+	7,
+	8,
+	9
+	]
+
+cube1 = [
+	1,
+	2,
+	3,
+	4,
+	8,
+	9
+	]
+
+sq = [
+	x * x |
+	x <- [1..9]
+	]
+
+perm = permutations $ cube0 ++ cube1
+
+
+distSq :: ([Int], [Int]) -> [Int]
+distSq (xs0, xs1) = xs0 ++ xs1
+
+main = print perm
