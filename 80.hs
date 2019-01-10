30f1a9ac1f990d3eae7c21813e002a23ca03b2e3
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-01-09 23:58:24 -0700

    30% of the way there

---
+++ 80.hs
@@ -0,0 +1,24 @@
+import Data.Char
+import Data.List
+import Data.List
+
+digiSum n = splitWhen (== '.') (take 100 (map (\x -> digitToInt x) (show n)))
+
+deci n = show n
+
+takeAway = [
+	x ^ 2 | 
+	x <- [1..10]
+	]
+
+no = [
+	x |
+	x <- [1..100]
+	]
+
+rng = [
+	sqrt x |
+	x <- no \\ takeAway
+	]
+
+main = print (deci (sqrt 7))
