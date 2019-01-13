6f12f4a75a1e333f1f310a5bc182f899d26d7ecd
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-01-12 23:05:35 -0700

    50% of the way there

---
+++ 77.hs
@@ -0,0 +1,27 @@
+import Data.List
+
+prime n = null [
+	x |
+	x <- [2..(n `div` 2)],
+	n `mod` x == 0
+	]
+
+fac n = [
+	x |
+	x <- [1..n],
+	n `mod` x == 0
+	]
+
+primeFac = [
+	x |
+	x <- [1..10],
+	prime x
+	]
+
+equals = [
+	x |
+	x <- permutations primeFac,
+	sum x == 10
+	]
+
+main = print (equals)
