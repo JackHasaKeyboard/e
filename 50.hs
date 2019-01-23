5984fd7868ea69ec5611f78965057fa9091a2118
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-01-22 23:33:17 -0700

    50% of the way there. Getting somewhere. Slowly albeit

---
+++ 50.hs
@@ -0,0 +1,18 @@
+prime n = null [
+	x |
+	x <- [2..n `div` 2],
+	n `mod` x == 0
+	]
+
+-- primeChainSum n = sum [
+-- 	x |
+-- 	x <- [2..n],
+-- 	prime x
+-- 	]
+
+main = print (
+	take 21 [
+		x |
+		x <- [2..],
+		prime x
+		])
