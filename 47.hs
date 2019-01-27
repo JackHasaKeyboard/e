a231c9e363956264f0efb3570e15d510cdad380a
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-01-27 00:53:09 -0700

    90% of the way there

---
+++ 47.hs
@@ -0,0 +1,28 @@
+fac n = [
+	x |
+	x <- [2..n `div` 2],
+	n `mod` x == 0
+	]
+
+prime n = null [
+	x |
+	x <- [2..n `div` 2],
+	n `mod` x == 0
+	]
+
+rmDup :: Eq a => [a] -> [a]
+rmDup = rdHelper []
+	where
+		rdHelper seen [] = seen
+		rdHelper seen (x : xs)
+			| x `elem` seen = rdHelper seen xs
+			| otherwise = rdHelper (seen ++ [x]) xs
+
+uniq n = [
+	rng |
+	x <- [1..],
+	let rng = [x..x + (n - 1)],
+	sum (concat (rmDup (map (\x -> fac x) rng))) >= (n * n)
+	]
+
+main = print (head (uniq 4))
