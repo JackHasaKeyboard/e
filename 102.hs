bc572bb4a3b776cce5f0f5b148488477871751f4
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-01-28 22:28:16 -0700

    10% of the way there. Not working, I just need to sleep

---
+++ 102.hs
@@ -0,0 +1,6 @@
+cont :: ((Int, Int) -> (Int, Int) -> (Int, Int)) -> String
+cont ((x0, y0), (x1, y1), (x2, y2)) = "asdf"
+
+-- cont (asdf, hjkl) = asdf
+
+main = print (cont (-340, 495), (-153, -910), (835, -947))
