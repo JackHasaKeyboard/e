e702e85e9413dbd4f3aeacf0338ddb7dd1c6843e
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-02-01 03:00:24 -0700

    36. Requires a bit of re-formatting

--- 36.hs
+++ 36.hs
@@ -1,27 +1,15 @@
-addDigit :: Int -> Int -> Int
-addDigit num d = 10 * num + d
+palin str = str == reverse str
 
-helper :: Int -> [Int]
-helper 0 = []
-helper n | n `mod` 2 == 1 = 1 : helper (n `div` 2)
-         | n `mod` 2 == 0 = 0 : helper (n `div` 2)
-         
-bin :: Int -> [Int]
-bin 0 = [0]
-bin n = reverse (helper n)
+bin 0 = []
+bin n
+	| n `mod` 2 == 0 = bin (n `div` 2) ++ [0]
+	| n `mod` 2 == 1 = bin (n `div` 2) ++ [1]
 
-fromDigits = foldl addDigit 0
-   where addDigit num d = 10*num + d
+fromDig = foldl addDig 0
+	where addDig no d = 10 * no + d
 
-rev :: Int -> Int
-rev n = read $ reverse $ show n
-
-pan :: Int -> Bool
-pan n = bin n == bin (rev n)
-
-duoPan :: Int -> Bool
-duoPan n = pan n == pan (fromDigits (bin n))
-
-ls = [x | x <- [1..1000], duoPan x]
-
-main = print $ pan $ fromDigits $ bin 73
+main = print (sum ([
+	x |
+	x <- [1..999999],
+	palin (show x) && palin (show (fromDig (bin x)))
+	]))
