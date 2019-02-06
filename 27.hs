74cbcfdd7ae42a21ae440027fc0e5ac387be53b4
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-02-06 00:07:31 -0700

    Made some progress

--- 27.hs
+++ 27.hs
@@ -1,9 +1,19 @@
 quad :: Int -> Int -> Int -> Int
-quad n a b = (n ^ 2) + (a * n) + b
+quad n a b = n ^ 2 + a * n + b
 
-main = print [
-	quad n a b |
-	a <- [1..1000],
-	b <- [1..1000],
-	n <- [0..]
+prime :: Int -> Bool
+prime n = null [
+	x |
+	x <- [2..n `div` 2],
+	n `mod` x == 0
 	]
+
+-- asdf = [
+-- 	quad n a b |
+-- 	n <- [1..10] :: [Int],
+-- 	a <- [1..10] :: [Int],
+-- 	b <- [1..10] :: [Int],
+-- 	all (prime) (quad n a b)
+-- 	]
+
+main = print (all (prime) (quad 1 3 7))
