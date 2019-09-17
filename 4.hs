eddafc8b4d0f979c75da4dba53017b3d06a534db
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-09-16 23:53:09 -0600

    Fix, format

--- 4.hs
+++ 4.hs
@@ -1,10 +1,10 @@
-rev :: Int -> Int
 rev = read . reverse . show
 
-palin :: Int -> Bool
 palin i = i == rev i
 
-main = do
-  let ls = [(x, y) | x <- [100..999], y <- [100..999], palin (x * y)]
-
-  print ls
+main = print (last [
+	(*) x y |
+	x <- [100..999],
+	y <- [100..999],
+	palin ((*) x y)
+	])
