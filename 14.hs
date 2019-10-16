278092dda9fb435cd945fb0d20826a4c00fbc4f3
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-10-15 20:51:44 -0600

    Format

--- 14.hs
+++ 14.hs
@@ -1,16 +1,14 @@
 import Data.List
 
-collatz :: Int -> Int
 collatz n
-	| even n = n `div` 2
-	| odd n = (n * 3) + 1
+	| even n = (div) n 2
+	| odd n = (+) ((*) n 3) 1
 
-sequ :: Int -> [Int]
 sequ n
 	| n == 1 = [1]
-	| otherwise = n:sequ (collatz n)
+	| otherwise = n : sequ (collatz n)
 
-main = do
-	let ls = [sequ x | x <- [1..999999]]
-
-	print $ tail $ sort ls
+main = print (tail (sort [
+	sequ x |
+	x <- [1..999999]
+	]))
