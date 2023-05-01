2b303e3a9dba7a030a074c60e081d919370df9d8
Jack <jackhasakeyboard@gmail.com>
2023-05-01 08:30:03 -0600

    Utilized proper function when getting head of list, previously using the one for a tuple

--- 5.hs
+++ 5.hs
@@ -2,6 +2,6 @@ fac :: Int -> [Int]
 fac n = [x | x <- [1..n], n `mod` x == 0]
 
 main = do
-	let mult = fst [x | x <- [20..], fac x == [1..20]]
+	let mult = head [x | x <- [20..], fac x == [1..20]]
 
 	print mult
