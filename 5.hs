23f654ad68afc21e351005520965f8a4e14377ad
Jack <jackhasakeyboard@gmail.com>
2023-05-01 08:32:45 -0600

    Broke list generated via list comprehension onto multiple lines

--- 5.hs
+++ 5.hs
@@ -1,7 +1,13 @@
 fac :: Int -> [Int]
-fac n = [x | x <- [1..n], n `mod` x == 0]
+fac n = [
+	x | x <- [1..n],
+	n `mod` x == 0
+	]
 
 main = do
-	let mult = head [x | x <- [20..], fac x == [1..20]]
+	let mult = head [
+		x | x <- [20..],
+		fac x == [1..20]
+		]
 
 	print mult
