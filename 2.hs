7836487d19c5dab1b9962b846c3a3b602255bc6a
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-26 18:20:35 -0600

    Removed declarations arbitrarily declared in main do block, update identifiers to not conflict with other builtins

--- 2.hs
+++ 2.hs
@@ -1,12 +1,10 @@
 fib :: Int -> Int -> [Int]
 fib a b = a:fib b (a + b)
 
-main = do
-	let
-		seq = [x | x <- take 4000000 (fib 0 1), x < 4000000]
+fibSeq = [x | x <- take 4000000 (fib 0 1), x < 4000000]
 
-		even = [x | x <- seq, x `mod` 2 == 0]
+fibEven = [x | x <- fibSeq, x `mod` 2 == 0]
 
-		total = sum even
+total = sum fibEven
 
-	print total
+main = print total
