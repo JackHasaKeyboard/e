8d517df5e38d13bcfa0b3f971b38337649393818
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-26 18:23:12 -0600

    Broke list comprehension onto multiple lines

--- 2.hs
+++ 2.hs
@@ -1,9 +1,15 @@
 fib :: Int -> Int -> [Int]
 fib a b = a:fib b (a + b)
 
-fibSeq = [x | x <- take 4000000 (fib 0 1), x < 4000000]
+fibSeq = [
+	x | x <- take 4000000 (fib 0 1),
+	x < 4000000
+	]
 
-fibEven = [x | x <- fibSeq, x `mod` 2 == 0]
+fibEven = [
+	x | x <- fibSeq,
+	x `mod` 2 == 0
+	]
 
 total = sum fibEven
 
