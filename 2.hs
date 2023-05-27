8cbb6e3f7c66ba65d6f02953357c03b62dc06dec
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-26 18:30:23 -0600

    Bound types in declarations like function

--- 2.hs
+++ 2.hs
@@ -1,16 +1,19 @@
 fib :: Int -> Int -> [Int]
 fib a b = a:fib b (a + b)
 
+fibSeq :: [Int]
 fibSeq = [
 	x | x <- take 4000000 (fib 0 1),
 	x < 4000000
 	]
 
+fibEven :: [Int]
 fibEven = [
 	x | x <- fibSeq,
 	x `mod` 2 == 0
 	]
 
+total :: Int
 total = sum fibEven
 
 main = print total
