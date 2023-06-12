0d6ae452a1fa1a393ecb63fba92f59da085a3d83
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:24:11 -0600

    Removed comment

--- 63.hs
+++ 63.hs
@@ -7,9 +7,3 @@ lenAndPow n = (length $ show n) == (isSq n n)
 
 main = do
 	print $ isSq 27 3
-
--- quint :: Int -> Bool
--- quint n = not $ null [x | x <- [1..n], x ^ 5 == n]
-
--- pow :: Int -> Bool
--- pow n = (length (show n) == n) && (quint n)
