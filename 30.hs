f0e0fcf48fb6f9284e60b701b8ce2224efb477d2
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:19:34 -0600

    Broke list comprehension onto multiple lines

--- 30.hs
+++ 30.hs
@@ -4,4 +4,7 @@ digit n = digit (n `div` 10) ++ [n `mod` 10]
 sum5thPw :: Int -> Bool
 sum5thPw n = n == (sum $ map (\x -> x ^ 5) $ digit n)
 
-main = print [x | x <- [1..], sum5thPw x]
+main = print [
+	x | x <- [1..],
+	sum5thPw x
+	]
