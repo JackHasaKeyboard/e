a3ac2998564b89de2cc998e1f0430e09d0dc95c3
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-12 00:15:02 -0600

    Broke list comprehension onto multiple lines

--- 21.hs
+++ 21.hs
@@ -1,13 +1,25 @@
 import Data.List (nub, sort)
 
 d :: Int -> Int
-d n = sum [x | x <- [1..n `div` 2], n `mod` x == 0]
+d n = sum [
+	x | x <- [1..n `div` 2],
+	n `mod` x == 0
+	]
 
 amic :: Int -> Int -> Bool
 amic x y = x == d y && y == d x
 
-ls = [[x, y] | x <- [1..10000], y <- [1..10000], amic x y]
+ls = [
+	[x, y] |
+	x <- [1..10000],
+	y <- [1..10000],
+	amic x y
+	]
 
-uniq = nub [sort x | x <- ls, x /= reverse x]
+uniq = nub [
+	sort x |
+	x <- ls,
+	x /= reverse x
+	]
 
 main = print uniq
