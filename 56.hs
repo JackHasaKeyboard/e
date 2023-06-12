6858738d57c7ae838652d85eadb84b8944fb5dd2
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:23:23 -0600

    Broke list comprehension onto multiple lines

--- 56.hs
+++ 56.hs
@@ -3,6 +3,9 @@ import Data.List
 
 total n = sum $ map (\x -> digitToInt x) (show n)
 
-nat = [total $ a ^ b | a <- [1..100], b <- [1..100]]
+nat = [
+	total $ a ^ b | a <- [1..100],
+	b <- [1..100]
+	]
 
 main = print $ sort nat
