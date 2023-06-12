f4f65bf216cb11c8cd48f7310de50c5f21863e0e
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:17:55 -0600

    Moved variables arbitrarily declared in main to global scope

--- 24.hs
+++ 24.hs
@@ -1,17 +1,17 @@
 import Data.List
 
-main = do
-	let digit = [
-		0,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9
-		]
+digit = [
+	0,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9
+	]
 
+main = do
 	print $ permutations digit !! 1000000
