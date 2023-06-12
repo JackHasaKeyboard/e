2c04ba961de31ef23824bb5f988970bc359382b4
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-12 00:10:34 -0600

    Removed non-functioning scripts

--- 200.hs
+++
@@ -1,7 +0,0 @@
-prime :: Int -> Bool
-prime n = null [x | x <- [1..n `div` 2], x `mod` n == 0]
-
-sqube :: (Int, Int) -> Bool
-sqube (p, q) = prime p && prime q && p /= q
-
-main = print $ sqube 200
--- 201.hs
+++
@@ -1,5 +0,0 @@
-import Data.List
-
-set = [1, 3, 6, 8, 10, 11]
-
-main = print $ permutations set
