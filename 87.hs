4985069cdbeecf351217a91b7fd49e0f203e3e60
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:32:06 -0600

    Broke list comprehension onto multiple lines

--- 87.hs
+++ 87.hs
@@ -1,7 +1,13 @@
 prime :: Int -> Bool
-prime n = null [x | x <- [1..(n `div` 2) - 1], n `mod` x == 0]
+prime n = null [
+	x | x <- [1..(n `div` 2) - 1],
+	n `mod` x == 0
+	]
 
 pow :: Int -> Int -> Bool
-pow n exp = null [x | x <- [1..n], x * exp == n]
+pow n exp = null [
+	x | x <- [1..n],
+	x * exp == n
+	]
 
 main = print $ pow 9 2
