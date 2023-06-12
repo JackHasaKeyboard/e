1e2db99789c17707d176b22756c27c6cd9dda0d9
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-12 00:17:49 -0600

    Relaced operator giving precedence with braces, removed arbitrary braces

--- 30.hs
+++ 30.hs
@@ -2,7 +2,7 @@ digit :: Int -> [Int]
 digit n = digit (n `div` 10) ++ [n `mod` 10]
 
 sum5thPw :: Int -> Bool
-sum5thPw n = n == (sum $ map (\x -> x ^ 5) $ digit n)
+sum5thPw n = n == sum (map (\x -> x ^ 5) (digit n))
 
 main = print [
 	x | x <- [1..],
