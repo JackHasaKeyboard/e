8c8866e361247ae25ea26d4b5f8beeceec078ae7
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-12 00:16:09 -0600

    Broke list comprehension onto multiple lines

--- 25.hs
+++ 25.hs
@@ -1,6 +1,9 @@
 fib :: Integer -> Integer -> [Integer]
 fib a b = a:fib b (a + b)
 
-large = [x | x <- fib 0 1, (length $ show x) == 1000]
+large = [
+	x | x <- fib 0 1,
+	(length $ show x) == 1000
+	]
 
 main = print $ head large
