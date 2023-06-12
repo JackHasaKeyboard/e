da34cbb6d1ccdf9bdff73130a0b6b398b6be2f47
jack-scrap <jackhasakeyboard@gmail.com>
2023-06-11 22:20:35 -0600

    Removed arbitrary braces around expression

--- 36.hs
+++ 36.hs
@@ -8,8 +8,8 @@ bin n
 fromDig = foldl addDig 0
 	where addDig no d = 10 * no + d
 
-main = print $ sum ([
+main = print $ sum [
 	x |
 	x <- [1..999999],
 	palin (show x) && palin (show (fromDig (bin x)))
-	])
+	]
