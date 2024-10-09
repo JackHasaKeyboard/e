bef6768fa34c8bbcd6ad74cfcf7c4573c7bae7c2
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-10-09 01:53:24 -0600

    Properly bound type as being integral

--- 14.hs
+++ 14.hs
@@ -1,9 +1,11 @@
 import Data.List
 
+collatz :: Int -> Int
 collatz n
 	| even n = n `div` 2
 	| odd n = (n * 3) + 1
 
+sequ :: Int -> [Int]
 sequ n
 	| n == 1 = [1]
 	| otherwise = n : sequ (collatz n)
